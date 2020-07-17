76e075fc40a502b03dd934a25a3ea0adb159bb45
Andres Miguez <andres.miguez@webbeds.com>
2020-07-17 13:10:36 +0200

    Updates

---
+++ precache-manifest.76c280638b8a23219af41170e4e460cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0b1c49505f9d736c3f8372eac848ae",
+    "url": "/react-material-ui/index.html"
+  },
+  {
+    "revision": "60f041bc72a5254c854a",
+    "url": "/react-material-ui/static/css/main.2ee2c65b.chunk.css"
+  },
+  {
+    "revision": "b1fbc765527d3e7d004e",
+    "url": "/react-material-ui/static/js/2.ef3995f6.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/react-material-ui/static/js/2.ef3995f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f041bc72a5254c854a",
+    "url": "/react-material-ui/static/js/main.0acfbb16.chunk.js"
+  },
+  {
+    "revision": "89f5a9cdd102631d984a",
+    "url": "/react-material-ui/static/js/runtime-main.de8be30c.js"
+  }
+]);
